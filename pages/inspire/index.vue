85e216e2e6c6cfbee6eefeb2561c021741e9dcb7
elenh <yisiwings@163.com>
2020-11-04 18:38:26 +0800

    update: toast plugin

--- pages/inspire/index.vue
+++ pages/inspire/index.vue
@@ -63,7 +63,9 @@ export default {
     }
   },
   mounted() {
-    this.$toasted.info('this is a msg.', { duration: 3000 })
+    this.$toasted.info('Yep!', { duration: 3000 })
+    this.$toast.info('this is a msg by toast.')
+    this.$toast.global.success()
   },
 }
 </script>
--- plugins/vue-toasted.js
+++ plugins/vue-toasted.js
@@ -1,4 +1,31 @@
 import Vue from 'vue'
 import Toasted from 'vue-toasted'
 
-Vue.use(Toasted)
+Vue.use(Toasted, {
+  duration: 5000,
+  position: 'top-center',
+  keepOnHover: true,
+  fitToScreen: false,
+  fullWidth: false,
+  iconPack: 'material',
+})
+
+const registers = [
+  {
+    name: 'success',
+    message: (payload) => {
+      const { message } = payload
+
+      return message || 'success...'
+    },
+    options: { type: 'success', icon: 'check' },
+  },
+]
+
+registers.forEach((v) => {
+  Vue.toasted.register(v.name, v.message, v.options)
+})
+
+export default (ctx, inject) => {
+  inject('toast', Vue.toasted)
+}
