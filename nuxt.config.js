12e8915927f50fb11e51ccbb7e1909aa40ffae64
elenh <yisiwings@163.com>
2021-01-07 19:54:24 +0800

    update: viewport -> user-scalable=no

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,7 +35,10 @@ export default {
     title: 'nuxt-starter-template',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1, user-scalable=no',
+      },
       { hid: 'description', name: 'description', content: '' },
     ],
     link: [
